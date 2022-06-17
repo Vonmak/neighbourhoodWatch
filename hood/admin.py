ea236141e5b8d73eda6590ab8554c14e25ccc341
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-17 09:29:37 +0300

    added the models to admin.

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Admin,Policedept,Healthdept, Neighbourhood,Business
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(Policedept)
+admin.site.register(Healthdept)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
