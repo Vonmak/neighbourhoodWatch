4fe19b7260b7628a36e2950226e76118f07c216f
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-17 17:10:39 +0300

    update forms and added the login and buiness form.

--- hood/forms.py
+++ hood/forms.py
@@ -3,18 +3,33 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import *
 
-class RegistrationForm( UserCreationForm, forms.ModelForm):
+class UserForm(forms.ModelForm):
     email=forms.EmailField(max_length=100, required=False)
     class Meta:
         model = User
-        fields = ('first_name','last_name','email','username','password1','password2')
+        fields = ('first_name','last_name','email','username','password','is_admin','is_profile')
 
 class AdminForm(forms.ModelForm):
     class Meta:
         model= Admin
-        fields= '__all__'
+        fields= ['house_number','phone',]
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        fields= ['house_number','phone',]
+   
         
 class HoodForm(forms.ModelForm):
     class Meta:
         model= Neighbourhood
         fields= '__all__'
+        
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model= Business
+        fields= '__all__'
+        
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=50)
+    password=forms.CharField(max_length=20, widget=forms.PasswordInput)
