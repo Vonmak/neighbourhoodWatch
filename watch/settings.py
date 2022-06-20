298ee1c83164fb2bddf81ee760c1db1e03111259
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-06-20 15:49:21 +0300

    configured the settings

--- watch/settings.py
+++ watch/settings.py
@@ -28,10 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!dqmf8k6lt25s%9p#o$9e_tpb9x4%js!l^3&9p=zyeboyp6whv'
+MODE=config("MODE", default="dev")
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['thehoodwatch.herokuapp.com', '.127.0.0.1']
 
